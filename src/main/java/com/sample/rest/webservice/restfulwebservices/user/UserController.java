57db5f6be05175b84bccb000e9c692b62a18fc34
Satabdi <satabdi.d@gmail.com>
2018-11-03 14:43:49 -0400

    Updates
    Updates

--- src/main/java/com/sample/rest/webservice/restfulwebservices/user/UserController.java
+++ src/main/java/com/sample/rest/webservice/restfulwebservices/user/UserController.java
@@ -1,13 +1,16 @@
 package com.sample.rest.webservice.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserController {
@@ -22,17 +25,19 @@ public class UserController {
 	public User findUser(@PathVariable String id) {
 		return dao.findUser(Integer.parseInt(id));
 	}
-	
+
 	@GetMapping("/users")
 	public List<User> getAllUser() {
 		return dao.findAll();
 	}
-	
-	@PostMapping("/users")
-	public User getUser(@RequestBody User user) {
-		User savedUser = dao.save(user);
-		return savedUser;
-	}
 
+	@PostMapping("/users")
+	public ResponseEntity getUser(@RequestBody User user) {
+		User savedUser = dao.save(user);
+		//CREATED
+		// /user/{id}
+		URI locaion = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(locaion).build();
+	}
 
 }
